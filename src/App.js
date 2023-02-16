59bb09cc9c37879495e13693596c248f972a29a0
rohitraman <rohitraman9944@gmail.com>
2023-02-16 20:00:15 +0530

    Added Landing Page and Dummy View Patients Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Receptionist } from './features';
+import { Header } from './components';
+import { Receptionist, RegisterPatient, ViewPatients } from './features';
 
 function App() {
   return (
     <div>
-      <Receptionist />  
+      <Header />
+      <Routes>
+        <Route path='/receptionist'>
+          <Route index element={<Receptionist />} />
+          <Route path='register' element={<RegisterPatient />} />
+          <Route path='view' element={<ViewPatients />} />
+        </Route>
+      </Routes>
     </div>
+    
   );
 }
 
---
+++ src/features/Receptionist/ViewPatients/ViewPatients.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ViewPatients.css';
+
+function ViewPatients() {
+    return (
+        <div>
+            View Patients
+        </div>
+    )
+}
+
+export default ViewPatients;
