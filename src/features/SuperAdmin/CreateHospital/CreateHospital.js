70d8d09b8ce7906a413fd5ee3db0f6aa788a8021
Rohith <rohithvaidya@gmail.com>
2023-02-18 22:21:06 +0530

    Font to Poppins -- Added to Constants File

--- src/features/SuperAdmin/CreateHospital/CreateHospital.js
+++ src/features/SuperAdmin/CreateHospital/CreateHospital.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form, Dropdown } from 'react-bootstrap';
 import { Textbox, Button, SuccessModal } from '../../../components';
 import './CreateHospital.css';
+import { constants } from '../../../constants';
 
 
 function CreateHospital() {
@@ -13,12 +14,12 @@ function CreateHospital() {
 
     return (
         <div className='sup-container'>
-            <p className='sup-heading'>Create Hospital Instance</p>
-            <p className='sup-second-heading'>Current Hospitals</p>
+            <p className='sup-heading'>{constants.REACT_APP_CREATE_HOSPITAL_HEADING_LABEL}</p>
+            <p className='sup-second-heading'>{constants.REACT_APP_CURRENT_HOSPITAL_LABEL}</p>
 
             <Dropdown className='sup-drop-down'>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Hospitals (By ID)
+                {constants.REACT_APP_HOSPITAL_DROPDOWN_LABEL}
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
@@ -30,14 +31,14 @@ function CreateHospital() {
 
             <Form>
                 <Form.Group className="mb-4 sup-group-style" >
-                    <Textbox label = "Hospital Name" type="text"/>
-                    <Textbox label = "Administrator Name" type="text"/>
-                    <Textbox label = "Administrator Email" type="email"/>
-                    <Textbox label = "Administrator Phone" type="phone"/>
+                    <Textbox label = {constants.REACT_APP_HOSPITAL_NAME_LABEL} type="text"/>
+                    <Textbox label = {constants.REACT_APP_ADMIN_NAME_LABEL} type="text"/>
+                    <Textbox label = {constants.REACT_APP_ADMIN_EMAIL_LABEL} type="email"/>
+                    <Textbox label = {constants.REACT_APP_ADMIN_PHONE_LABEL} type="phone"/>
                 </Form.Group>
                 
                 <div className='sup-register-button'>
-                    <Button label = "Create Hospital" onClick={(e) => onRegister(e)}/>
+                    <Button label = {constants.REACT_APP_CREATE_HOSPITAL_LABEL} onClick={(e) => onRegister(e)}/>
                 </div>
                 <SuccessModal show = {show} onButtonClick={setShow}/>
             </Form>
